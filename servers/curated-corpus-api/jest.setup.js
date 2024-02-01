9cd8290f781f41b71149269ec3e010a0884fdece
Katerina Chinnappan <katchinnapp@gmail.com>
2024-01-31 16:28:21 -0800

    updating node version to v20 (#26)
    * updating node version for monorepo
    
    * updating circleci node version

--- servers/curated-corpus-api/jest.setup.js
+++ servers/curated-corpus-api/jest.setup.js
@@ -2,3 +2,7 @@
 // this file is to provide a value when running tests outside of the container
 process.env.AWS_ENDPOINT = process.env.AWS_ENDPOINT || 'http://localhost:4566';
 jest.setTimeout(8000);
+// https://github.com/facebook/react-native/issues/35701#issuecomment-1847579429
+Object.defineProperty(global, 'performance', {
+    writable: true,
+});
