b735627d79365081e3f5552b6b81b1bbcec7c212
Mathijs Miermans <mmiermans@users.noreply.github.com>
2024-02-23 13:22:32 -0800

    feat: [MC-726] Allow GCP ML user write access to scheduler SQS (#74)
    * chore: run format to fix indentation
    
    * feat: Add scheduler SQS permissions for ML user
    
    * fix: remove permissionsBoundary from ML user
    
    This is implicitely preventing any other policies from being used.

--- infrastructure/corpus-scheduler-lambda/src/main.ts
+++ infrastructure/corpus-scheduler-lambda/src/main.ts
@@ -3,38 +3,43 @@ import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
 import { LocalProvider } from '@cdktf/provider-local/lib/provider';
 import { NullProvider } from '@cdktf/provider-null/lib/provider';
 import { Construct } from 'constructs';
-import {
-    App,
-    S3Backend,
-    TerraformStack,
-} from 'cdktf';
+import { App, S3Backend, TerraformStack } from 'cdktf';
 
-import {
-    PocketVPC,
-} from '@pocket-tools/terraform-modules';
+import { PocketVPC } from '@pocket-tools/terraform-modules';
 import { config } from './config';
-import {CorpusSchedulerSQSLambda} from "./corpusSchedulerLambda";
+import { CorpusSchedulerSQSLambda } from './corpusSchedulerLambda';
+import { MlIamUserPolicy } from './iam';
 
 class CorpusSchedulerLambdaWraper extends TerraformStack {
-    constructor(scope: Construct, name: string) {
-        super(scope, name);
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
 
-        new AwsProvider(this, 'aws', { region: 'us-east-1' });
-        new NullProvider(this, 'null-provider');
-        new LocalProvider(this, 'local-provider');
-        new ArchiveProvider(this, 'archive-provider');
+    new AwsProvider(this, 'aws', { region: 'us-east-1' });
+    new NullProvider(this, 'null-provider');
+    new LocalProvider(this, 'local-provider');
+    new ArchiveProvider(this, 'archive-provider');
 
-        new S3Backend(this, {
-            bucket: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
-            dynamodbTable: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
-            key: config.name,
-            region: 'us-east-1',
-        });
+    new S3Backend(this, {
+      bucket: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
+      dynamodbTable: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
+      key: config.name,
+      region: 'us-east-1',
+    });
 
-        const pocketVPC = new PocketVPC(this, 'pocket-vpc');
+    const pocketVPC = new PocketVPC(this, 'pocket-vpc');
 
-        new CorpusSchedulerSQSLambda(this, 'corpus-scheduler-sqs-lambda', pocketVPC);
-    }
+    const sqsLambda = new CorpusSchedulerSQSLambda(
+      this,
+      'corpus-scheduler-sqs-lambda',
+      pocketVPC,
+    );
+
+    new MlIamUserPolicy(
+      this,
+      'corpus-scheduler-ml-user-policy',
+      sqsLambda.sqsQueue,
+    );
+  }
 }
 const app = new App();
 new CorpusSchedulerLambdaWraper(app, 'corpus-scheduler-lambda-wrapper');
