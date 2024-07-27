6b35d9fa416772d3805e9f20c61b7ac4e7b439e8
Jonathan Petto (they/them) <jpetto@users.noreply.github.com>
2024-07-27 08:12:11 -0500

    Revert "chore: disable otlp in corpus API to reduce log noise while debugging…" (#197)
    This reverts commit e977f7d8045de70275c2ff0b122bcec5d296f376.

--- servers/curated-corpus-api/src/main.ts
+++ servers/curated-corpus-api/src/main.ts
@@ -1,28 +1,27 @@
+// nodeSDKBuilder must be the first import!!
+import {
+  nodeSDKBuilder,
+  AdditionalInstrumentation,
+} from '@pocket-tools/tracing';
+
+import config from './config';
 import { serverLogger } from '@pocket-tools/ts-logger';
 
-import {
-  curatedCorpusEventEmitter,
-  initItemEventHandlers,
-} from './events/init';
+nodeSDKBuilder({
+  host: config.tracing.host,
+  serviceName: config.tracing.serviceName,
+  release: config.sentry.release,
+  logger: serverLogger,
+  additionalInstrumentations: [AdditionalInstrumentation.PRISMA],
+}).then(async () => {
+  // Initialize event handlers, this is outside server setup as tests
+  // mock event handling
+  initItemEventHandlers(curatedCorpusEventEmitter, [
+    corpusItemSnowplowEventHandler,
+    corpusScheduleSnowplowEventHandler,
+    eventBusHandler,
+  ]);
 
-import {
-  corpusItemSnowplowEventHandler,
-  corpusScheduleSnowplowEventHandler,
-  eventBusHandler,
-} from './events/eventHandlers';
-
-import { startServer } from './express';
-import config from './config';
-
-// Initialize event handlers, this is outside server setup as tests
-// mock event handling
-initItemEventHandlers(curatedCorpusEventEmitter, [
-  corpusItemSnowplowEventHandler,
-  corpusScheduleSnowplowEventHandler,
-  eventBusHandler,
-]);
-
-async () => {
   const { adminUrl, publicUrl } = await startServer(config.app.port);
 
   serverLogger.info(
@@ -32,4 +31,17 @@ async () => {
   serverLogger.info(
     `🚀 Admin server is ready at http://localhost:${config.app.port}${adminUrl}`,
   );
-};
+});
+
+// the rest of the imports need to come *after* initializing the nodeSDKBuilder
+// above, as it monkey patches code
+import {
+  curatedCorpusEventEmitter,
+  initItemEventHandlers,
+} from './events/init';
+import {
+  corpusItemSnowplowEventHandler,
+  corpusScheduleSnowplowEventHandler,
+  eventBusHandler,
+} from './events/eventHandlers';
+import { startServer } from './express';
