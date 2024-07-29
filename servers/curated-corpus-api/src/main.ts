e7fe280c2e6a001bf042376c8292727f9381178f
Jonathan Petto <jpetto@users.noreply.github.com>
2024-07-29 09:42:15 -0500

    chore: disable otel
    - otel is flooding the logs and possibly spiking memory
    - this is likely a temporary disabling while we investigate

--- servers/curated-corpus-api/src/main.ts
+++ servers/curated-corpus-api/src/main.ts
@@ -1,27 +1,28 @@
-// nodeSDKBuilder must be the first import!!
-import {
-  nodeSDKBuilder,
-  AdditionalInstrumentation,
-} from '@pocket-tools/tracing';
-
-import config from './config';
 import { serverLogger } from '@pocket-tools/ts-logger';
 
-nodeSDKBuilder({
-  host: config.tracing.host,
-  serviceName: config.tracing.serviceName,
-  release: config.sentry.release,
-  logger: serverLogger,
-  additionalInstrumentations: [AdditionalInstrumentation.PRISMA],
-}).then(async () => {
-  // Initialize event handlers, this is outside server setup as tests
-  // mock event handling
-  initItemEventHandlers(curatedCorpusEventEmitter, [
-    corpusItemSnowplowEventHandler,
-    corpusScheduleSnowplowEventHandler,
-    eventBusHandler,
-  ]);
+import {
+  curatedCorpusEventEmitter,
+  initItemEventHandlers,
+} from './events/init';
 
+import {
+  corpusItemSnowplowEventHandler,
+  corpusScheduleSnowplowEventHandler,
+  eventBusHandler,
+} from './events/eventHandlers';
+
+import { startServer } from './express';
+import config from './config';
+
+// Initialize event handlers, this is outside server setup as tests
+// mock event handling
+initItemEventHandlers(curatedCorpusEventEmitter, [
+  corpusItemSnowplowEventHandler,
+  corpusScheduleSnowplowEventHandler,
+  eventBusHandler,
+]);
+
+(async () => {
   const { adminUrl, publicUrl } = await startServer(config.app.port);
 
   serverLogger.info(
@@ -31,17 +32,4 @@ nodeSDKBuilder({
   serverLogger.info(
     `🚀 Admin server is ready at http://localhost:${config.app.port}${adminUrl}`,
   );
-});
-
-// the rest of the imports need to come *after* initializing the nodeSDKBuilder
-// above, as it monkey patches code
-import {
-  curatedCorpusEventEmitter,
-  initItemEventHandlers,
-} from './events/init';
-import {
-  corpusItemSnowplowEventHandler,
-  corpusScheduleSnowplowEventHandler,
-  eventBusHandler,
-} from './events/eventHandlers';
-import { startServer } from './express';
+})();
