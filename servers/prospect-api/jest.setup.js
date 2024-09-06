2246862227db0ff80cbeefa1cdfe80b6e79f53a5
Jonathan Petto <jpetto@users.noreply.github.com>
2024-09-06 13:49:40 -0500

    moving prospect-api to GHA

--- servers/prospect-api/jest.setup.js
+++ servers/prospect-api/jest.setup.js
@@ -1,9 +1,12 @@
-// AWS_ENDPOINT is set in .docker/local.env
-// this file is to provide a value when running tests outside of the container
 process.env.AWS_ENDPOINT = process.env.AWS_ENDPOINT || 'http://localhost:4566';
+process.env.AWS_ACCESS_KEY_ID = 'localstack-fake-id';
+process.env.AWS_DEFAULT_REGION = 'us-east-1';
+process.env.AWS_REGION = 'us-east-1';
+process.env.AWS_SECRET_ACCESS_KEY = 'localstack-fake-key';
+
 jest.setTimeout(8000);
+
 // https://github.com/facebook/react-native/issues/35701#issuecomment-1847579429
 Object.defineProperty(global, 'performance', {
-    writable: true,
+  writable: true,
 });
-
