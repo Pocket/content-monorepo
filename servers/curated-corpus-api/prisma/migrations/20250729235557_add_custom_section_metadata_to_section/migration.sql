183e68612dee428ed7825f8cba2f400d4982085a
Mathijs Miermans <m.miermans@gmail.com>
2025-07-30 16:39:45 -0700

    Revert "HNT-874: Add custom section metadata to Section prisma schema"
    This reverts commit b6d34b1db7fb2492850d908af661a9a969eb524b.

--- servers/curated-corpus-api/prisma/migrations/20250729235557_add_custom_section_metadata_to_section/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Section` ADD COLUMN `description` VARCHAR(255) NULL,
-    ADD COLUMN `endDate` DATE NULL,
-    ADD COLUMN `heroDescription` VARCHAR(255) NULL,
-    ADD COLUMN `heroTitle` VARCHAR(255) NULL,
-    ADD COLUMN `startDate` DATE NULL;
